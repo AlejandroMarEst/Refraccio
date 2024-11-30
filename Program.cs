11963d0c821172fe1ca44f9920fe77815ae9f31e
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2024-11-30 22:57:10 +0100

    Posat el codi a metodes Closes #3

--- Program.cs
+++ Program.cs
@@ -2,6 +2,32 @@
 
 class Program
 {
+    static double CalculateRectangleArea(double width, double height)
+    {
+        return width * height;
+    }
+    static double CalculateCircleCircumference(double radius)
+    {
+        return 2 * Math.PI * radius;
+    }
+    static void CheckInterval(double area)
+    {
+        const string AreaBigger = "L'àrea és més gran de 50";
+        const string AreaInside = "L'àrea és entre 20 i 50";
+        const string AreaSmaller = "L'àrea és menor o igual a 20";
+        if (area > 50)
+        {
+            Console.WriteLine(AreaBigger);
+        }
+        else if (area > 20)
+        {
+            Console.WriteLine(AreaInside);
+        }
+        else
+        {
+            Console.WriteLine(AreaSmaller);
+        }
+    }
     static void Main(string[] args)
     {
         // Sol·licita l'entrada de l'usuari per calcular l'àrea d'un rectangle
@@ -10,36 +36,21 @@ class Program
         const string AreaRectangle = "L'àrea del rectangle és: {0}";
         const string InputRadius = "Introdueix el radi del cercle:";
         const string CircumferenceCircle = "La circumferència del cercle és: {0}";
-        const string AreaBigger = "L'àrea és més gran de 50";
-        const string AreaInside = "L'àrea és entre 20 i 50";
-        const string AreaSmaller = "L'àrea és menor o igual a 20";
+
         Console.WriteLine(InputWidth);
         double widthRectangle = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine(InputHeight);
         double heightRectangle = Convert.ToDouble(Console.ReadLine());
 
         // Calcula l'àrea
-        double areaRectangle = widthRectangle * heightRectangle;
+        double areaRectangle = CalculateRectangleArea(widthRectangle, heightRectangle);
         Console.WriteLine(AreaRectangle, areaRectangle);
 
         // Sol·licita l'entrada de l'usuari per calcular la circumferència d'un cercle
         Console.WriteLine(InputRadius);
         double radiusCircle = Convert.ToDouble(Console.ReadLine());
-        double circumferenceCircle = 2 * Math.PI * radiusCircle;
+        double circumferenceCircle = CalculateCircleCircumference(radiusCircle);
         Console.WriteLine(CircumferenceCircle, circumferenceCircle);
-
-        // Imprimeix un missatge basat en el valor de l'àrea
-        if (areaRectangle > 50)
-        {
-            Console.WriteLine(AreaBigger);
-        }
-        else if (areaRectangle > 20)
-        {
-            Console.WriteLine(AreaInside);
-        }
-        else
-        {
-            Console.WriteLine(AreaSmaller);
-        }
+        CheckInterval(areaRectangle);
     }
 }
